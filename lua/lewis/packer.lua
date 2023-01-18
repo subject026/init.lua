0b0c6e583a5fe4143ea3db18e4070f6c475ed39d
subject026 <subject026@protonmail.com>
2023-01-18 11:44:28 +0000

    fix packer config

--- lua/lewis/packer.lua
+++ lua/lewis/packer.lua
@@ -28,27 +28,25 @@ return require('packer').startup(function(use)
   use('tpope/vim-fugitive')
 
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},
-    -- Snippet Collection (Optional)
-    {'rafamadriz/friendly-snippets'},
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      -- Snippet Collection (Optional)
+      {'rafamadriz/friendly-snippets'},
+    }
   }
-}
- use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
   end)
